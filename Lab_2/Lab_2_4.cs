c87d08a3d1f7c6e5d3948b5dd86d711a36eecfc9
Shashidhar <shashidhar.gandi@gmail.com>
2022-08-23 13:42:54 +0530

    Create Lab_2_4.cs

---
+++ Lab_2/Lab_2_4.cs
@@ -0,0 +1,30 @@
+
+using System;
+
+namespace App {
+
+    //class ProductDemo
+    //{
+    //    Object PID,PName,Price,Qty;
+    //    double amtPay;
+    //    ProductDemo()
+    //    {
+    //        PID = new Object();
+    //    }
+    //}
+    class main {
+        public static void Main(){
+
+            int i = 90;
+
+            object k = i;
+            
+            k = (int)i;
+            
+            Console.ReadLine();
+
+        }
+
+    }
+    
+}
