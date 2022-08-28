39542985d085507f3e69b9ca3dbc8f632faccbd2
Shashidhar <shashidhar.gandi@gmail.com>
2022-08-28 14:12:24 +0530

    Update Lab_5_1.cs
    Finished Project with modifications...

--- Lab_5/Lab_5_1.cs
+++ Lab_5/Lab_5_1.cs
@@ -1,6 +1,12 @@
 using System;
 
 namespace BankApp{
+    
+    public enum BankAccountTypeEnum{  
+            Current=1,
+            Saving=2
+    }
+    
     interface IBankAccount{
         
          double GetBalance();
@@ -11,19 +17,92 @@ namespace BankApp{
 
     }
     
-    public enum  BankAccountTypeEnum
-    {  
-        Current=1,
-        Saving=2
+    abstract class BankAccount : IBankAccount{
+        
+        public BankAccountTypeEnum AccountType {get; set;}
+        
+        protected double Balance = 0.0;
+        
+        public double GetBalance(){
+            return this.Balance;
+        }
+        
+        public void Deposit(double amount){
+            this.Balance += amount;
+        }
+        
+        public abstract bool Withdraw(double amount);
+        public abstract bool Transfer(IBankAccount toAccount, double amount);
+        
+        
     }
     
-    abstract class BankAccount : IBankAccount{
-        protected double Balance;
+    class ICICI : BankAccount{
+        
+        
+        public override bool Withdraw(double amount){
+            if(this.Balance - amount>=0){
+                this.Balance -=amount;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        public override bool Transfer(IBankAccount toAccount, double amount){
+            if(this.Balance - amount>=1000){
+                this.Balance -=amount;
+                Console.WriteLine("Transfering Money : " + amount);
+                toAccount.Deposit(amount);
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    
+    class HSBC : BankAccount{
+        public override bool Withdraw(double amount){
+            if(this.Balance - amount>=0){
+                this.Balance -=amount;
+                
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        public override bool Transfer(IBankAccount toAccount, double amount){
+            if(this.Balance - amount>=1000){
+                this.Balance -=amount;
+                Console.WriteLine("Transfering Money : " + amount);
+                toAccount.Deposit(amount);
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
     
     class main{
         
         public static void Main(){
+            
+            BankAccount acc1 = new ICICI();
+            BankAccount acc2 = new HSBC();
+            
+            acc1.Deposit(100000);
+            acc2.Deposit(200000);
+            
+            acc1.Transfer(acc2,10000);
+            acc2.Transfer(acc1,20000);
+            
+            Console.WriteLine(acc1.GetBalance());
+            Console.WriteLine(acc2.GetBalance());
             
         }
     }
